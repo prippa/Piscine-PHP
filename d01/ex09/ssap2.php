b4ea9fc858ccbb0418d16afa4380966f22a08047
prippa <pavelrippa@gmail.com>
2018-04-04 02:17:00 +0300

    d01->ex09 update

--- d01/ex09/ssap2.php
+++ d01/ex09/ssap2.php
@@ -1,30 +1,5 @@
 #!/usr/bin/env php
 <?php
-	// function cmp($left, $right) {
-	// 	$i = 0;
-
-	// 	while ($i < strlen($left) && $i < strlen($right))
-	// 	{
-			
-	// 	}
-	// }
-
-	// function extrim_sort($array) {
-	// 	for ($i = 0; $i < count($array); $i++)
-	// 	{
-	// 		for ($j = 0; $j < count($array) - 1; $j++)
-	// 		{
-	// 			if (cmp($array[$j], $array[$j + 1]))
-	// 			{
-	// 				$tmp = $array[$j];
-	// 				$array[$j] = $array[$j + 1];
-	// 				$array[$j + 1] = $tmp;
-	// 			}
-	// 		}
-	// 	}
-	// 	return ($array);
-	// }
-
 	function ft_split($str) {
 		$arr = explode(' ', $str);
 		$arr = array_values(array_filter($arr));
@@ -42,45 +17,41 @@
 		return ($arr);
 	}
 
-	function ccmp($c1, $c2) {
-		$c1 = strtolower($c1);
-		$c2 = strtolower($c2);
-		if (ctype_alpha($c1))
-			$g1 = 1;
-		else if (ctype_digit($c1))
-			$g1 = 2;
+	function getCharPriority($c) {
+		if (ctype_alpha($c))
+			$priority = 1;
+		else if (ctype_digit($c))
+			$priority = 2;
 		else
-			$g1 = 3;
-		if (ctype_alpha($c2))
-			$g2 = 1;
-		else if (ctype_digit($c2))
-			$g2 = 2;
-		else
-			$g2 = 3;
-		if ($g1 != $g2)
-			return ($g1 - $g2);
-		return (strcmp($c1, $c2));
+			$priority = 3;
+		return ($priority);
 	}
 
-	function cmp($str1, $str2) {
-		while ($j < strlen($str1) && $j < strlen($str2))
+	function extrimSort($a, $b) {
+		if ($a == $b)
+			return (0);
+		$a = strtolower($a);
+		$b = strtolower($b);
+		while ($i < strlen($a) && $i < strlen($b))
 		{
-			if (ccmp($str1[$j], $str2[$j]) > 0)
+			$pri_a = getCharPriority($a[$i]);
+			$pri_b = getCharPriority($b[$i]);
+			if ($pri_a == $pri_b)
+				$res = strcmp($a[$i], $b[$i]);
+			else
+				$res = $pri_a - $pri_b;
+			if ($res > 0)
 				return (1);
-			if (ccmp($str1[$j], $str2[$j]) < 0)
+			else if ($res < 0)
 				return (-1);
-			$j++;
+			$i++;
 		}
-		if ($j < strlen(str1))
-			return (1);
-		if ($j < strlen(str2))
-			return (-1);
-		return (0);
+		return (strcmp($a, $b));
 	}
 
 	array_shift($argv);
 	$arr = ft_ssap($argv);
-	usort($arr, "cmp");
+	usort($arr, "extrimSort");
 	for ($i = 0; $i < count($arr); $i++)
 		echo "$arr[$i]\n";
 ?>
